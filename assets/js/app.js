3366cc02a4f3c0e2026eabcb2feb1c804f16961f
DannyRux <dangorlov@yahoo.com>
2017-10-07 11:49:45 -0400

    Functional Javascript

--- assets/js/app.js
+++ assets/js/app.js
@@ -12,16 +12,45 @@ var config = {
 };
 firebase.initializeApp(config);
 
-var database = firebase.database();
 
-function Employee(name, role, startDate, monthlyRate) {
+
+var database = firebase.database();
+/*
+var name = "";
+var start = "";
+var rate = 0;
+var role = "";
+*/
+
+function Employee(name, role, start, rate) {
     this.name = name;
     this.role = role;
-    this.startDate = startDate;
-    this.monthlyRate = monthlyRate;
-    this.dateAdded = firebase.database.ServerValue.TIMESTAMP;
+    this.startDate = start;
+    this.monthlyRate = rate;
+    this.dateAdded = firebase.database.ServerValue.TIMESTAMP
 }
 
+database.ref().on("child_added", function(snapshot){ 
+    var display = $("#employee-display");
+    var tr = $("<tr>")
+        .append(`<td>${ snapshot.val().name }</td>`)
+        .append(`<td>${ snapshot.val().role }</td>`)
+        .append(`<td>${ snapshot.val().startDate }</td>`)
+        .append(`<td></td>`)
+        .append(`<td>$${snapshot.val().monthlyRate }</td>`)
+        .append(`<td></td>`);
 
-database.ref().push(new Employee("Mike", "Front End Designer", "9/20/17", "45000"));
+        
+    display.append(tr);
+});
 
+$("#add-employee-btn").on('click', function(e) {
+    e.preventDefault();
+    var name = $("#employee-name-input").val().trim();
+    var start = $("#start-input").val().trim();
+    var rate = $("#rate-input").val().trim();
+    var role = $("#role-input").val().trim();
+    
+    database.ref().push(new Employee(name, role, start, rate));
+
+});
