f9127039eba6c566ba1bbfd4b4554121a5220d3c
Margaret Rumble <megrumble@gmail.com>
2017-10-07 11:12:47 -0400

    momentjs

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyDHYPSOVl1kepVUMSyVyFg68ny5Yj5DBl4",
+    authDomain: "employeetimesheet-58aae.firebaseapp.com",
+    databaseURL: "https://employeetimesheet-58aae.firebaseio.com",
+    projectId: "employeetimesheet-58aae",
+    storageBucket: "",
+    messagingSenderId: "887508912266"
+  };
+  firebase.initializeApp(config);
+</script>
